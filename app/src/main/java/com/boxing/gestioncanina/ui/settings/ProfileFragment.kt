c135ac59c9486d2c751f995483699dfdb36aab3a
BryanDevop <bryanjoelbatistaramirez@gmail.com>
2025-11-14 09:11:28 -0400

    Nada

--- app/src/main/java/com/boxing/gestioncanina/ui/settings/ProfileFragment.kt
+++ app/src/main/java/com/boxing/gestioncanina/ui/settings/ProfileFragment.kt
@@ -1,21 +1,36 @@
 package com.boxing.gestioncanina.ui.settings
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.boxing.gestioncanina.R
+import com.boxing.gestioncanina.ui.dashboard.DashboardActivity
+
 
 class ProfileFragment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_profile_fragment)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // 🔹 Buscar el botón
+        val btnBack = findViewById<ImageButton>(R.id.btnBack)
+
+        // 🔹 Acción al hacer clic
+        btnBack.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+            finish() // opcional: cierra esta Activity
         }
     }
 }
